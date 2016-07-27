096536f7dc466bfa783833fd261281756b3aa1c3
crane.liu <crane.liu@qq.com>
2016-07-27 08:08:15 +0800

    添加返回顶部支持

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,41 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+var CustomJs = function () {
+ // handle go to top button
+    var handleGo2Top = function () {       
+        var Go2TopOperation = function(){
+            var CurrentWindowPosition = $(window).scrollTop();// current vertical position
+            if (CurrentWindowPosition > 300) {
+                $(".go2top").show();
+            } else {
+                $(".go2top").hide();
+            }
+        };
+
+        Go2TopOperation();// call headerFix() when the page was loaded
+        if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+            $(window).bind("touchend touchcancel touchleave", function(e){
+                Go2TopOperation();
+            });
+        } else {
+            $(window).scroll(function() {
+                Go2TopOperation();
+            });
+        }
+
+        $(".go2top").click(function(e) {
+            e.preventDefault();
+             $("html, body").animate({ scrollTop: 0 }, 600);
+        });
+    }
+	return {
+        init: function () {
+			handleGo2Top();
+		}
+	};
+}();
+
+jQuery(document).ready(function() {    
+   CustomJs.init();
+});
